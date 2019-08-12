b6c57caa51e5c1b58791bceaa87478ad134a427f
Taku Akahane <taku.akahane@gmail.com>
2019-08-12 17:46:25 +0900

    mod

---
+++ app/models/singleton/using_singleton.rb
@@ -0,0 +1,5 @@
+ClassBasedLogger.level = ClassBasedLogger::INFO
+
+ClassBasedLogger.info('コンピュータがチェスゲームに勝ちました')
+ClassBasedLogger.warning('ユニットAE-35の故障が予測されました')
+ClassBasedLogger.error('HAL-900機能停止、緊急動作を実行します')
